77a59e2b4c2d6ac94b00f91fee8019fbeafe424e
swwan <wsw90@126.com>
2021-12-18 17:14:33 +0800

    feat: 类型转换工具类
    转换Object为指定泛型，错误打印完整堆栈信息

---
+++ research-common/src/main/java/com/swwan/research/common/utils/cast/CastUtils.java
@@ -0,0 +1,25 @@
+package com.swwan.research.common.utils.cast;
+
+import cn.hutool.json.JSONUtil;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 类型转换工具类
+ *
+ * @author swwan
+ * @date 2021/12/18 - 16:57
+ */
+@Slf4j
+public class CastUtils {
+
+    @SuppressWarnings("unchecked")
+    public static <T> T castObject2Type(Object obj) {
+        try {
+            return (T) obj;
+        } catch (ClassCastException e) {
+            log.error("类型转换出错：source = [{}]", JSONUtil.toJsonStr(obj));
+            log.error("完整堆栈信息：", e);
+            return null;
+        }
+    }
+}
