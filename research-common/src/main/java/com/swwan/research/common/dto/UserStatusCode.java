b9bc582b768365a034b5eaed845b0ce543069f7c
swwan <wsw90@126.com>
2021-12-19 00:13:50 +0800

    feat: 可扩展性状态码

---
+++ research-common/src/main/java/com/swwan/research/common/dto/UserStatusCode.java
@@ -0,0 +1,26 @@
+package com.swwan.research.common.dto;
+
+import com.sun.org.apache.xerces.internal.dom.PSVIAttrNSImpl;
+import com.swwan.research.common.base.IStatusCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * 扩展的状态码
+ */
+@Getter
+@AllArgsConstructor
+@SuppressWarnings("all")
+public enum UserStatusCode implements IStatusCode {
+    ERR_2000(2000, "用户信息不存在"),
+    ERR_2001(2001, "用户昵称格式错误"),
+    ;
+
+    private Integer status;
+    private String msg;
+
+    public static void main(String[] args) {
+        IStatusCode userStatusCode = UserStatusCode.ERR_2000;
+        System.out.println(userStatusCode.getStatus()+":"+userStatusCode.getMsg());
+    }
+}
