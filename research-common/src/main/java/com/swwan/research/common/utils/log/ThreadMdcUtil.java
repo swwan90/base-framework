2a519648d1fc4b73d82dd6cb11e8635a8519498a
swwan <wsw90@126.com>
2021-12-19 22:43:13 +0800

    feat: 日志基于MDC实现traceId——线程池traceId跟踪

---
+++ research-common/src/main/java/com/swwan/research/common/utils/log/ThreadMdcUtil.java
@@ -0,0 +1,58 @@
+package com.swwan.research.common.utils.log;
+
+import cn.hutool.core.util.IdUtil;
+import com.swwan.research.common.constant.Constants;
+import org.slf4j.MDC;
+
+import java.util.Map;
+import java.util.concurrent.Callable;
+
+/**
+ * 线程MDC包装工具类
+ *
+ * @author swwan
+ * @date 2021/12/19 - 22:24
+ */
+public class ThreadMdcUtil {
+
+    /**
+     * 线程设置traceId
+     */
+    public static void setTraceIdIfAbsent() {
+        if (MDC.get(Constants.TRACE_ID) == null) {
+            MDC.put(Constants.TRACE_ID, IdUtil.getSnowflake().nextIdStr());
+        }
+    }
+
+    public static <T> Callable<T> wrap(final Callable<T> callable, final Map<String, String> context) {
+        return () -> {
+            if (context == null) {
+                MDC.clear();
+            } else {
+                MDC.setContextMap(context);
+            }
+            setTraceIdIfAbsent();
+            try {
+                return callable.call();
+            } finally {
+                MDC.clear();
+            }
+        };
+    }
+
+    public static Runnable wrap(final Runnable runnable, final Map<String, String> context) {
+        return () -> {
+            if (context == null) {
+                MDC.clear();
+            } else {
+                MDC.setContextMap(context);
+            }
+            setTraceIdIfAbsent();
+            try {
+                runnable.run();
+            } finally {
+                MDC.clear();
+            }
+        };
+    }
+}
---
+++ research-common/src/main/java/com/swwan/research/common/utils/wrapper/ThreadPoolExecutorMdcWrap.java
@@ -0,0 +1,48 @@
+package com.swwan.research.common.utils.wrapper;
+
+import com.swwan.research.common.utils.log.ThreadMdcUtil;
+import org.slf4j.MDC;
+
+import java.util.concurrent.*;
+
+/**
+ * @author swwan
+ * @date 2021/12/19 - 22:33
+ */
+public class ThreadPoolExecutorMdcWrap extends ThreadPoolExecutor {
+    public ThreadPoolExecutorMdcWrap(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue) {
+        super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue);
+    }
+
+    public ThreadPoolExecutorMdcWrap(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue, ThreadFactory threadFactory) {
+        super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, threadFactory);
+    }
+
+    public ThreadPoolExecutorMdcWrap(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue, RejectedExecutionHandler handler) {
+        super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, handler);
+    }
+
+    public ThreadPoolExecutorMdcWrap(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue, ThreadFactory threadFactory, RejectedExecutionHandler handler) {
+        super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, threadFactory, handler);
+    }
+
+    @Override
+    public void execute(Runnable command) {
+        super.execute(ThreadMdcUtil.wrap(command, MDC.getCopyOfContextMap()));
+    }
+
+    @Override
+    public Future<?> submit(Runnable task) {
+        return super.submit(ThreadMdcUtil.wrap(task, MDC.getCopyOfContextMap()));
+    }
+
+    @Override
+    public <T> Future<T> submit(Runnable task, T result) {
+        return super.submit(ThreadMdcUtil.wrap(task, MDC.getCopyOfContextMap()), result);
+    }
+
+    @Override
+    public <T> Future<T> submit(Callable<T> task) {
+        return super.submit(ThreadMdcUtil.wrap(task, MDC.getCopyOfContextMap()));
+    }
+}
