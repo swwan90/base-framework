16821c349750183f5c1025c6f4cf5632b956b285
swwan90 <wsw90@126.com>
2022-03-04 22:41:12 +0800

    feat: 代码优化-消灭if-else
    创建module，和badCode

---
+++ research-demo/src/java/com/swwan/research/demo/perf/bean/TestCodeData.java
@@ -0,0 +1,24 @@
+package com.swwan.research.demo.perf.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * @author swwan
+ * @date 2022/3/4 - 22:32
+ */
+@Data
+public class TestCodeData {
+
+    private Integer city;
+
+    private Integer isHoliday;
+
+    private Integer startTime;
+
+    private Integer endTime;
+
+    private Object TimeUnit;
+
+
+}
