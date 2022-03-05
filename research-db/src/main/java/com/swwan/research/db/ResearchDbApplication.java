373360af6db3fff360bdc2f00aadf0f3adb8834d
swwan90 <wsw90@126.com>
2022-03-05 23:02:55 +0800

    build: DB 学习
    搭建模块

---
+++ research-db/src/main/java/com/swwan/research/db/ResearchDbApplication.java
@@ -0,0 +1,16 @@
+package com.swwan.research.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author swwan
+ * @date 2022/1/16 - 18:20
+ */
+@SpringBootApplication
+public class ResearchDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResearchDbApplication.class, args);
+    }
+}
